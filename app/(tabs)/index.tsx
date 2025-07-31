a15afec1c25c633001befdfeb19811029edb9c43
Manuwave <ejehemmanuel019@gmail.com>
2025-07-31 16:15:46 +0100

    learning components

--- app/(tabs)/index.tsx
+++
@@ -1,7 +0,0 @@
-import { View,Text } from "react-native";
-
-export default function Index() {
-  return (
-    <View><Text>my self edited code</Text></View>
-  )
-}
---
+++ components/custom-styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const customStyles = StyleSheet.create({
+    main:{
+        backgroundColor: "oldlace",
+        paddingHorizontal:16,
+        
+    }
+
+})
